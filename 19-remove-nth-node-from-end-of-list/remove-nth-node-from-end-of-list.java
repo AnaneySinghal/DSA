106f145e263881dc71a2e9b93ae52fe259b3c88d
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-08-09 20:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.65%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null) { return null; }
+       // if (head == null) { return null; }
         ListNode slow=head;
         ListNode fast=head;
 
