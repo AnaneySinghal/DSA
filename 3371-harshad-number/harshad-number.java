18e97fbabad8b0909e3c344206ee083af2166b6c
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-08-09 17:44:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (44.78%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+
+        int original=x;
+        while(x!=0){
+            int ld=x%10;
+            sum+=ld;
+            x=x/10;
+
+          
+        }
+
+          if(original%sum==0) {
+            return sum;
+        }
+        return -1;
+        
+        
+    }
+}
