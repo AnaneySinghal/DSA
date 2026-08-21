4ff6eeacccd54bf1b9b39b1f9ecf9153a0c84143
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-08-21 21:31:11 +0530

    Time: 2 ms (67.33%) | Memory: 85.1 MB (80.09%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean inc =true;
+        boolean dec=true;
+
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]) dec=false;
+            if(nums[i]>nums[i+1]) inc =false;
+        }
+        if(inc==true || dec==true) return true;
+        return false;
+    }
+}
