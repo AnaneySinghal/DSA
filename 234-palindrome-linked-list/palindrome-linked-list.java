c3da616716b806ed1253145fb31e590c4e464a00
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-08-29 10:25:22 +0530

    Time: 4 ms (67.23%) | Memory: 94 MB (94.42%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -23,6 +23,8 @@ class Solution {
         return prev;
     }
     public boolean isPalindrome(ListNode head) {
+         if (head == null || head.next == null)
+            return true;
         ListNode slow=head;
         ListNode fast=head;
 
