a2e8891d5c2fcf1ad1d1b0d1cd9d0f9d747904a4
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-04 12:56:51 +0530

    Time: 17 ms (9.57%) | Memory: 41.8 MB (93.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,6 +1,15 @@
 class Solution {
+    static int[] dp;
+    public int fibo(int n){
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans=fibo(n-1)+fibo(n-2);
+        return ans;
+    }
     public int fib(int n) {
-        double ans=(1+Math.sqrt(5))/2;
-        return (int) Math.round(Math.pow(ans,n)/Math.sqrt(5));
+        dp=new int[n+1];
+       return fibo(n);
+
+        
     }
 }
