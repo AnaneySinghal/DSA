9a8158db8d0e2fae3a2b4239b765c045af558c8a
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-04 17:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.25%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int[] dp;
+    public int rob(int[] nums) {
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return loot(0,nums);
+        
+    }
+    public int loot(int idx,int[] nums){
+        if(idx>=nums.length) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        int pick=nums[idx]+loot(idx+2,nums);
+        int skip=loot(idx+1,nums);
+        int ans=Math.max(skip,pick);
+        dp[idx]=ans;
+        return ans;
+    }
+}
