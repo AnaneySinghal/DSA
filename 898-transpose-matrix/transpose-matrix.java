d374e9f6fe16ab50198b0626f6d15b317a6e550c
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-05 17:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (11.46%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -2,9 +2,9 @@ class Solution {
     public int[][] transpose(int[][] nums) {
         int[][] ans = new int[nums[0].length][nums.length];
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums[0].length;j++){
-                ans[j][i]=nums[i][j];
+        for(int i=0;i<ans.length;i++){
+            for(int j=0;j<ans[0].length;j++){
+                ans[i][j]=nums[j][i];
             }
         }
         return ans;
