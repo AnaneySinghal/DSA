d5ba5f6ecebb00324175d9ec3dc684179f33a6ad
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-06 10:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (62.24%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,19 @@
 class Solution {
-    public boolean binarySearch(int[][]nums, int target , int row ){
+    public boolean searchMatrix(int[][] nums, int target) {
         int low=0;
-        int high=nums[0].length-1;
+        int high=(nums.length * nums[0].length)-1;
+
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[row][mid]==target) return true;
-            else if(nums[row][mid]>target) high=mid-1;
+
+            int row=mid/nums[0].length;
+            int col=mid%nums[0].length;
+
+            if(nums[row][col]==target) return true;
+            else if(nums[row][col]>target) high=mid-1;
             else low=mid+1;
         }
         return false;
-    }
-
-    public boolean searchMatrix(int[][] nums, int target) {
-        int n=nums.length;
-        int m=nums[0].length;
-
-        int start_row=0;
-        int end_row=n-1;
-
-        while(start_row<=end_row){
-            int mid_row=start_row+(end_row-start_row)/2;
-
-            if(target>=nums[mid_row][0] && target<=nums[mid_row][m-1]){
-                return binarySearch(nums,target,mid_row);
-            }
-            else if(target>nums[mid_row][m-1]){
-                start_row=mid_row+1;
-            }
-            else{
-                end_row=mid_row-1;
-            }
-        }
-        return false;
+        
     }
 }
