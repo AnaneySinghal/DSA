d3b53b8a34b9aa49905c92f268c0defdfffd22b2
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-06 17:29:37 +0530

    Time: 5 ms (17.99%) | Memory: 269.1 MB (5.87%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,4 +1,12 @@
 class Solution {
+    public void rotate(int[] arr, int k) {
+        int n=arr.length;
+        k=k%n;
+        reverse(0,n-1,arr);
+        reverse(0,k-1,arr);
+        reverse(k,n-1,arr); 
+        
+    }
      public void reverse(int i , int j , int[]arr){
         while(i<=j){
             int temp=arr[i];
@@ -7,14 +15,5 @@ class Solution {
             i++;
             j--;
         }
-    }
-    public void rotate(int[] arr, int k) {
-        int n=arr.length;
-        k=k%n;
-        reverse(0,n-1,arr);
-        reverse(0,k-1,arr);
-        reverse(k,n-1,arr);
-        
-        
     }
 }
