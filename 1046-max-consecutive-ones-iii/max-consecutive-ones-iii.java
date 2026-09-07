3fc4e7736cf826066ffb2e5f49419b844c8dac70
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-07 12:33:30 +0530

    Time: 2 ms (99.91%) | Memory: 52.3 MB (35.68%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -4,9 +4,14 @@ class Solution {
        
         int count_zero=0;
 
+ // r ko 0 se n-1 tk chalao or jese hi 0 mile
+ // count_zero ko increase kro
         for(int r=0;r<nums.length;r++){
             if(nums[r]==0) count_zero++;
         
+// count_zero jese hi k se bda ho jaye left ko bhadao
+// or jese hi left ko 0 mile count ko decrease kro
+// last me r pointer n k equal hi jaega or widnow ki len n-left k equal ho jayegi
         if(count_zero>k){
             if(nums[left]==0) count_zero--;
             left++;
