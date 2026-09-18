275cc665df64e6cf18747562494b4e465b63eea0
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-18 21:08:28 +0530

    Time: 2 ms (63.58%) | Memory: 43.5 MB (49.85%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
-        StringBuilder sb =new StringBuilder(s.concat(s));
-         return sb.toString().contains(goal);
+        if (s.length() != goal.length()) {
+            return false;
+        }
 
-        
+        String doubled = s + s;
+
+        return doubled.contains(goal);
     }
-}
+} 
