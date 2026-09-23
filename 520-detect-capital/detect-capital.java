dd490cd91fdad787c376acb82a40e3f8ceda565e
AnaneySinghal <ananey.singhal_cs23@gla.ac.in>
2026-09-23 21:13:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (11.89%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean detectCapitalUse(String s) {
+        int upperCase=0;
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+            if(Character.isUpperCase(ch)) upperCase++;
+        }
+
+        if(upperCase==0) return true;
+        if(upperCase==s.length()) return true;
+        if(upperCase==1 && Character.isUpperCase(s.charAt(0))) return true;
+        return false;
+        
+    }
+}
